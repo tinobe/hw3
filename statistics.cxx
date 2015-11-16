29df5305923c604482db2745c2e10f80406f76af
Konstantin Beyer <kobey100@pc06.pool.tp1.uni-duesseldorf.de>
2015-11-16 19:14:51 +0100

    Program for mean value and variance of an array

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,32 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void fill(double* p, const int N){
+for(int i=0;i<N;i++) p[i]=rand()/double(RAND_MAX);
+}
+
+void calc(double* p, const int N, double& mean, double& var){
+   for(int i=0; i<N; i++) mean=mean+p[i]; // sum
+   mean=mean/N; var=pow((p[0]-mean),2);  // divide by N
+   for(int i=1; i<N; i++) var= var+ pow((p[i]-mean),2);  // sum
+   var=var/N;   // divide by N
+}
+
+void print(double* p, const int N){
+   for(int i=0; i<N; i++) cout << p[i] << endl;
+}
 
 int main(){
    const int N = 100;
    double p[N];
-   double mean, var;
+   double mean=0, var=0;
 
-   // Some lines here....
+   srand(time(NULL));
+
+   fill(p,N);  //  fill the array
+   //print(p,N);  to check
+   calc(p,N,mean,var);  // calculate the parameters
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
